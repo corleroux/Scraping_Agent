e2c3a749409cb67daf59247ba599a457bdf449d7
Cor le Roux <corleroux@gmail.com>
2024-06-08 05:21:19 +0200

    Adding web-rag

--- scrapers/web-rag.py
+++ scrapers/web-rag.py
@@ -9,3 +9,39 @@ loader = FireCrawlLoader(
     mode="crawl",  # Mode set to 'crawl' to crawl all accessible subpages
 )
 docs = loader.load()
+
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+vectorstore = FAISS.from_documents(documents=splits, embedding=OllamaEmbeddings())
+
+
+question = "What is firecrawl?"
+docs = vectorstore.similarity_search(query=question)
+
+
+from groq import Groq
+
+client = Groq(
+    api_key="YOUR_GROQ_API_KEY",
+)
+
+completion = client.chat.completions.create(
+    model="llama3-8b-8192",
+    messages=[
+        {
+            "role": "user",
+            "content": f"You are a friendly assistant. Your job is to answer the users question based on the documentation provided below:\nDocs:\n\n{docs}\n\nQuestion: {question}",
+        }
+    ],
+    temperature=1,
+    max_tokens=1024,
+    top_p=1,
+    stream=False,
+    stop=None,
+)
+
+print(completion.choices[0].message)
